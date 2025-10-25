dfab04a028464c280c7abc19f2374dcb758c484b
UniMark <104282848+unipresent@users.noreply.github.com>
2025-10-26 01:25:01 +0200

    Update filter_feed.py

--- filter_feed.py
+++ filter_feed.py
@@ -3,30 +3,21 @@ import requests
 
 URL = "https://www.unipresent.cz/google-merchant/1831"
 
-# Stáhni XML jako čistý text
-response = requests.get(URL, timeout=300)
-response.encoding = "utf-8"
-feed = response.text
+# stáhni celý XML feed
+r = requests.get(URL, timeout=300)
+r.encoding = "utf-8"
+feed = r.text
 
-# Najdi všechny bloky <item>...</item>
+# vyber pouze <item> s dostupností in stock
 items = re.findall(r"<item[\s\S]*?</item>", feed)
+instock = [x for x in items if "<g:availability>in stock</g:availability>" in x.lower()]
 
-# Vyfiltruj pouze ty, které jsou 'in stock'
-in_stock = [
-    i for i in items
-    if re.search(r"<g:availability>\s*in stock\s*</g:availability>", i, re.I)
-]
+header = feed.split("<item>")[0]
+footer = "</channel></rss>"
 
-# Najdi hlavičku a patičku (včetně XML deklarace)
-header_match = re.search(r"^.*?<channel>", feed, re.S)
-footer_match = re.search(r"</channel>\s*</rss>\s*$", feed, re.S)
+# slož výsledek
+xml_data = header + "".join(instock) + footer
 
-header = header_match.group(0) if header_match else '<?xml version="1.0" encoding="utf-8"?><rss xmlns:g="http://base.google.com/ns/1.0" version="2.0"><channel>'
-footer = footer_match.group(0) if footer_match else "</channel></rss>"
-
-# Slož nové XML (doslova)
-filtered_feed = header + "\n" + "\n".join(in_stock) + "\n" + footer
-
-# Ulož jako UTF-8, bez převodu tagů
-with open("feed_instock.xml", "wb") as f:
-    f.write(filtered_feed.encode("utf-8"))
+# uložit přesně tak, jak je (žádná konverze)
+with open("feed_instock.xml", "w", encoding="utf-8", newline="") as f:
+    f.write(xml_data)
